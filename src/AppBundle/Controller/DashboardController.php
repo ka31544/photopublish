aa308770f062b75513ef597483f20c37a2dfc6bb
Adam Kanigowski <ka31544@zut.edu.pl>
2019-12-05 21:35:34 +0100

    Status History sort by date

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $statusHistory = $this->getDoctrine()->getManager()->getRepository('AppBundle:StatusHistory')->findAll();
+        $statusHistory = $this->getDoctrine()->getManager()->getRepository('AppBundle:StatusHistory')->findBy([], ['date'=>'DESC']);
 
         return $this->render('dashboard/index.html.twig', array(
             'statusHistory' => $statusHistory,
